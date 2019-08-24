f18dabb971cf0541f0ae29a753a3991c8c92f794
wangjinliang <@huobi.com>
2019-08-24 23:16:39 +0800

    pass

--- app-service-config.js
+++ app-service-config.js
@@ -5,5 +5,17 @@ module.exports = {
   main: './index.js',
   server: "http://localhost:6110",
   cloudHub: "http://localhost:6080?username=test&password=123456",
-  generate: {}
+  generate: {
+    host: "192.168.3.46",
+    user: "root",
+    port: "3306",
+    password: "password",
+    database: "ada-uc",
+    charset: "utf8",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    consumer: ['pink'],
+    output: require("path").resolve(__dirname, "./g")
+  }
 };
